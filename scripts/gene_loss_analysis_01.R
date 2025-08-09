2d5d431f4c9c6ad3803fd594ffc06a27f2ce3bdc
chinson03 <charliescotthinson@gmail.com>
2025-08-09 18:30:01 +0100

    test commit

--- scripts/gene_loss_analysis_01.R
+++ scripts/gene_loss_analysis_01.R
@@ -13,7 +13,7 @@ anage <- read.delim("data/anage_data.txt")
 ## Generating dataframe, sourcing regeneration genes and traits from https://ngdc.cncb.ac.cn/regeneration/index
 ## Using binary categories for simplicity
 regen_traits <- data.frame("Class" <- anage$Class,"Genus" <- anage$Genus, "Species" <- anage$Species, "Common.name" <- anage$Common.name, "Regen_present" <- rep(0, length(anage$Species)), "Limb_regen" <- rep(0, length(anage$Species)), "Oocyte_regen" <- rep(0, length(anage$Species)), "Tooth_replace" <- rep(0, length(anage$Species)), "Indeterminate_growth" <- rep(0, length(anage$Species)), "Negligible_senescence" <- rep(0, length(anage$Species)))
-## Create CSV
+## Create CSVgit pu
 write.csv(regen_traits, "data/regen_traits.csv", row.names = FALSE)
 ## Searching spp. for regeneration keyword
 ## Separating amphibians for https://amphibiaweb.org/
@@ -21,3 +21,4 @@ write.csv(regen_traits, "data/regen_traits.csv", row.names = FALSE)
 amphibians <- regen_traits[regen_traits$X.class.....anage.Class == "Amphibia",]
 
 ## Reptiles from https://reptile-database.reptarium.cz/search?search=regeneration&submit=Search
+## Test edit for committing
