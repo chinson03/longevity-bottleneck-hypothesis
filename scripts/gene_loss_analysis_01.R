a1391aa61fb92a6bd2258fc81d7bdbea27d39e8a
chinson03 <charliescotthinson@gmail.com>
2025-08-10 17:12:46 +0100

    removed redundant data, imported whole set as one

--- scripts/gene_loss_analysis_01.R
+++ scripts/gene_loss_analysis_01.R
@@ -7,7 +7,9 @@ library(ggtree)
 library(rvest)
 library(httr)
 library(jsonlite)
-
+library(tidyr)
+library(purrr)
+library(xml2)
 ## Lifespan + Ageing traits
 anage <- read.delim("data/anage_data.txt")
 
@@ -19,32 +21,17 @@ anage <- read.delim("data/anage_data.txt")
 ## In this database, only 6 spp. are listed. Will need to find alternative source for if regen is present in any given species.
 ## 2. Limb regeneration
 ## Combining bone, muscle, and cartilage data for limb_regen
-bone_genes <- read.csv("data/bone_regen_genes.csv", header = FALSE)
-muscle_genes <- read.csv("data/muscle_regen_genes.csv", header = FALSE)
-cart_genes <- read.csv("data/cart_regen_genes.csv", header = FALSE)
-
 ## 3. Oocyte regen
 ## 4. Tooth replacement
-tooth_genes <- read.csv("data/tooth_Regen_genes.csv", header = FALSE)
-
 ## 5. Indeterminate growth
 ## 6. Negligible senescence
 
+gen_age <- read.csv("data/genage_models.csv", header = TRUE)
+head(gen_age)
 
-
-
-regen_genes <- data.frame(
-  gene = character(),
-  species = character(),
-  category = character(),
-  source = character(),
-  stringsAsFactors = FALSE
-)
-
-regen_genes <- rbind(regen_genes, data.frame(
-  gene = 
-))
-
+regen_genes <- data.frame(read.csv("data/regen_genes.csv", header = TRUE))
+unique(regen_genes$Species)
+head(regen_genes)
 ## Sourcing regen traits from amphibian + reptilian databases
 ## Creating dataframe
 ## Using binary categories for simplicity
@@ -55,6 +42,19 @@ write.csv(regen_traits, "data/regen_traits.csv", row.names = FALSE)
 ## Searching spp. for regeneration keyword
 ## Separating amphibians for https://amphibiaweb.org/
 ## https://amphibiaweb.org/amphib_dump.xml
+amphibians <- read_xml("data/amphib_dump.xml")
+## Take species nodes
+species_nodes <- xml_find_all(amphibians, ".//species")
+## Extract text if present
+get_node_text <- function(node, xpath) {
+  child <- xml_find_first(node, xpath)
+  if (length(child) > 0) xml_text(child) else NA_character_
+}
+
+xml_structure(amphibians)
+
+
+
 amphibians <- regen_traits[regen_traits$X.class.....anage.Class == "Amphibia",]
 
 
